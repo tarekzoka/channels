97b7b91edb3cbe6905cf4de97d600c604d32e289
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-09 02:53:25 +0200

    Add files via upload

---
+++ hussein-chanell-X.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+##
+echo
+rm -f /etc/defaultsat.tar.gz && rm -f /etc/enigma2/lamedb && rm -f /etc/enigma2/*.tv && rm -f /etc/enigma2/*.radio && rm -f /etc/enigma2/*.del && rm -f /etc/enigma2/*.sh && wait
+
+wget "https://raw.githubusercontent.com/tarekzoka/channels/main/a.hussein-motor.tar.gz"
+
+
+tar -xzf a.hussein-motor.tar.gz  -C /
+
+wait
+rm -f /tmp/a.hussein-motor.tar.gz
+echo "   UPLOADED BY  >>>>   TAREK_TT "   
+sleep 4;                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo BY-TAREK-HANFY   "**********************************************************************************"
+wait
+killall -9 enigma2
+exit 0
+
+###################################
+#                                 #
+#           BY-TAREK-HANFY        #              
+#                                 #
+###################################
