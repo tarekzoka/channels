5df286474686b84d8fd89c00c378ff5eb5391744
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-05-10 06:03:42 +0300

    Add files via upload

--- channel-ahmed-husin.sh
+++ channel-ahmed-husin.sh
@@ -12,27 +12,17 @@ wait
 
 wget "https://raw.githubusercontent.com/tarekzoka/channels/main/ahmed-husin-6-5-2024.tar.gz"
 
-
 tar -xzf ahmed-husin-6-5-2024.tar.gz  -C /
 
-wait
 rm -f /tmp/ahmed-husin-6-5-2024.tar.gz
-echo "   UPLOADED BY  >>>>   TAREK_TT "   
-sleep 4;                                                                                                                  
-echo ". >>>>         RESTARING     <<<<"
-echo BY-TAREK-HANFY   "**********************************************************************************"
+
+echo "   UPLOADED BY  >>>>   TAREK_HANFY"   
 echo
 echo "   >>>>   Reloading Services - Please Wait   <<<<"
 wget -qO - http://127.0.0.1/web/servicelistreload?mode=0 > /dev/null 2>&1
+sleep 2
+echo
+init 3
 
-wait
-
-killall -9 enigma2
 exit 0
-
-###################################
-#                                 #
-#           BY-TAREK-HANFY        #              
-#                                 #
-###################################
 
