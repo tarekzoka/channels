367050efad3af9dda77b504d36d875531270c891
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-06-30 03:22:06 +0300

    Add files via upload

--- channel-Quran-iptv.sh
+++ channel-Quran-iptv.sh
@@ -8,9 +8,6 @@ tar -xzf QURAN.tar.gz  -C /
 rm -f /tmp/QURAN.tar.gz
 
 echo "   UPLOADED BY  >>>>   TAREK_HANFY"   
-echo
-echo "   >>>>   Reloading Services - Please Wait   <<<<"
-wget -qO - http://127.0.0.1/web/servicelistreload?mode=0 > /dev/null 2>&1
 sleep 2
 echo
 init 3
