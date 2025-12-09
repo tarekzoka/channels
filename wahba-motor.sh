f73747efcccf3f6f8ca8ff53e0c66cfa7b510506
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2025-12-09 19:39:16 +0200

    Add files via upload

---
+++ wahba-motor.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+##
+echo ""
+
+rm -rf /etc/enigma2/lamedb
+rm -rf /etc/enigma2/*list
+rm -rf /etc/enigma2/*.tv
+rm -rf /etc/enigma2/*.radio
+rm -rf /etc/tuxbox/*.xml
+
+wait
+
+wget "https://raw.githubusercontent.com/tarekzoka/channels/main/h.wahba-motor.tar.gz"
+
+
+tar -xzf h.wahba-motor.tar.gz  -C /
+
+wait
+rm -f /tmp/h.wahba-motor.tar.gz
+echo "   UPLOADED BY  >>>>   TAREK_TT "   
+sleep 4;                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo BY-TAREK-HANFY   "**********************************************************************************"
+echo
+echo "   >>>>   Reloading Services - Please Wait   <<<<"
+wget -qO - http://127.0.0.1/web/servicelistreload?mode=0 > /dev/null 2>&1
+
+wait
+
+killall -9 enigma2
+exit 0
+
+###################################
+#                                 #
+#           BY-TAREK-HANFY        #              
+#                                 #
+###################################
+
